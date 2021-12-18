ee91adef949e6f2cd24fd0af363364706a41852e
kanishka_bandara <wmkubandara@gmail.com>
2021-12-18 16:17:01 +0530

    ADD::Entire Project and this is the first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Election Automation System for Sri Lanka
---
+++ src/test/java/com/nipun/election/ElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipun.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
