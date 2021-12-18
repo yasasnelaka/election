5c850a855047f52ec7356f010a1fb87f9c2866ca
kanishka_bandara <wmkubandara@gmail.com>
2021-12-18 18:51:29 +0530

    ADD::WebSocket for notification feature

---
+++ src/main/java/com/nipun/election/notification/ResponseMessage.java
@@ -0,0 +1,20 @@
+package com.nipun.election.notification;
+
+public class ResponseMessage {
+    private String content;
+
+    public ResponseMessage() {
+    }
+
+    public ResponseMessage(String content) {
+        this.content = content;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/nipun/election/notification/WsTutorialApplication.java
@@ -0,0 +1,4 @@
+package com.nipun.election.notification;
+
+public class WsTutorialApplication {
+}
