feab22889a8492a936c6cb9cdd75528e375579ec
kanishka_bandara <wmkubandara@gmail.com>
2021-12-27 10:34:55 +0530

    ADD::Basic configorations and citizen list without finalizing

---
+++ src/main/java/com/nipun/election/init/URLHolder.java
@@ -0,0 +1,7 @@
+package com.nipun.election.init;
+
+public class URLHolder {
+    public static String LOGIN_VIEW = "/login";
+    public static String LOGIN_REQUEST = "/login-request";
+    public static String CITIZEN_LIST_VIEW = "/citizen-list";
+}
---
+++ src/main/java/com/nipun/election/init/ViewHolder.java
@@ -0,0 +1,6 @@
+package com.nipun.election.init;
+
+public class ViewHolder {
+    public static String LOGIN_VIEW = "login";
+    public static String CITIZEN_LIST_VIEW = "citizens-list";
+}
