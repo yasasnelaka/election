4c4e53e5ba55f3acc4e0328e2b5f44f3a27882d1
kanishka_bandara <wmkubandara@gmail.com>
2021-12-28 21:11:29 +0530

    ADD::Voting form interface

--- src/main/java/com/nipun/election/controllers/web/VoteController.java
+++ src/main/java/com/nipun/election/controllers/web/VoteController.java
@@ -1,12 +1,15 @@
 package com.nipun.election.controllers.web;
 
+import com.nipun.election.models.responseModels.PageDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VoteController {
-    @GetMapping("voting-form")
-    public String index(){
+    @GetMapping("/voting-form")
+    public String index(Model model){
+        model.addAttribute("page_details", new PageDetails("EVS | Voting Form", "Voting Form"));
         return "voting-form";
     }
 }
