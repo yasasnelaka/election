50e568f7ab5afe278b81a28be8a474a8b936ebee
kanishka_bandara <wmkubandara@gmail.com>
2021-12-29 08:34:24 +0530

    ADD::Election Final Result View

--- src/main/java/com/nipun/election/init/ViewHolder.java
+++ src/main/java/com/nipun/election/init/ViewHolder.java
@@ -5,4 +5,5 @@ public class ViewHolder {
     public static String CITIZEN_LIST_VIEW = "citizens-list";
     public static String CANDIDATES_LIST_VIEW = "candidates-list";
     public static String ELECTIONS_LIST_VIEW = "elections-list";
+    public static String ELECTION_RESULTS_VIEW = "election-results";
 }
