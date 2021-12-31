d337dfb19d9816f56316c3c41bdde82cdc3d1cc5
kanishka_bandara <wmkubandara@gmail.com>
2021-12-31 14:58:21 +0530

    ADD::Entities

---
+++ src/main/java/com/nipun/election/dbTier/entities/Citizen.java
@@ -0,0 +1,55 @@
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "citizen")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Citizen {
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "polling_division_id")
+    private Integer pollingDivisionId;
+
+    @Column(name = "nic")
+    private String nic;
+
+    @Column(name = "full_name")
+    private String fullName;
+
+    @Column(name = "gender")
+    private java.lang.Byte gender;
+
+    @Column(name = "birthday")
+    private java.sql.Date birthday;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "mobile")
+    private String mobile;
+
+    @Column(name = "address")
+    private String address;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @Column(name = "status")
+    private Integer status;
+
+}
